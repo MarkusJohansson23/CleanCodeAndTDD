69f37f538c7c4708ea3c5bcb25fa6b07178cc1bd
MarkusJohansson23 <77443853+MarkusJohansson23@users.noreply.github.com>
2021-12-20 11:35:38 +0100

    Add project files.

---
+++ CleanCodeAndTDD/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeAndTDD
+{
+    public static class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
---
+++ TestStringCalculator/UnitTest1.cs
@@ -0,0 +1,21 @@
+using CleanCodeAndTDD;
+using Xunit;
+
+namespace TestStringCalculator
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Should_Return_Zero_If_Input_Is_Empty()
+        {
+            var result = StringCalculator.Add("");
+            Assert.Equal(0, result);
+        }
+        //[Fact]
+        //public void Should_Return_One_If_Input_Is_One()
+        //{
+        //    var result = StringCalculator.Add("1");
+        //    Assert.Equal(1, result);
+        //}
+    }
+}
